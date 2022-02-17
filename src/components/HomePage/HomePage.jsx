c40470428e9b97212726bc6c92f2a24b3fc51710
Bennett Smrdel <smrdelb@gmail.com>
2022-02-17 13:37:38 -0600

    added a sticky navbar whenever you scroll far enough

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../Header/Header';
+import CompanyServices from '../CompanyServices/CompanyServices';
 
 function HomePage() {
     return (
         <>
             <Header />
+            <CompanyServices />
         </>
     );
 }
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -29,34 +29,44 @@ function Nav() {
     setContactActive(true);
   }
 
+  window.addEventListener('scroll', function(ev) {
+    console.log('window', window.scrollY);
+    if (window.scrollY >= 420) {
+      document.getElementById("navbar").classList.add("sticky-top");
+    }
+    if (window.scrollY <= 400) {
+      document.getElementById("navbar").classList.remove("sticky-top");
+    }
+  });
+
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#" onClick={handleSelectHome} draggable={false}><span id='sh-logo'>SH</span> Compliance & Service Consulting</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0 nav-container">
-            {homeActive ? 
-              <Link className="nav-link active-tab" to="/home" draggable={false}>Our Services</Link> 
-            : 
-              <Link className="nav-link" to="/home" onClick={handleSelectHome} draggable={false}>Our Services</Link>
-            }
-            {aboutActive ?
-              <Link className="nav-link active-tab" to="/about" draggable={false}>About</Link>
-            :
-              <Link className="nav-link" to="/about" onClick={handleSelectAbout} draggable={false}>About</Link>
-            }
-            {contactActive ?
-              <Link className="nav-link active-tab" to="/contact" draggable={false}>Contact</Link>
-            :
-              <Link className="nav-link" to="/contact" onClick={handleSelectContact} draggable={false}>Contact</Link>
-            }
-          </ul>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light" id='navbar'>
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#" onClick={handleSelectHome} draggable={false}><span id='sh-logo'>SH</span> Compliance & Service Consulting</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 nav-container">
+              {homeActive ? 
+                <Link className="nav-link active-tab" to="/home" draggable={false}>Our Services</Link> 
+              : 
+                <Link className="nav-link" to="/home" onClick={handleSelectHome} draggable={false}>Our Services</Link>
+              }
+              {aboutActive ?
+                <Link className="nav-link active-tab" to="/about" draggable={false}>About</Link>
+              :
+                <Link className="nav-link" to="/about" onClick={handleSelectAbout} draggable={false}>About</Link>
+              }
+              {contactActive ?
+                <Link className="nav-link active-tab" to="/contact" draggable={false}>Contact</Link>
+              :
+                <Link className="nav-link" to="/contact" onClick={handleSelectContact} draggable={false}>Contact</Link>
+              }
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
   );
 }
 
