987ca2769f821a87835f7034744843c312df55bb
Bennett Smrdel <smrdelb@gmail.com>
2022-02-17 10:21:22 -0600

    fixed the sh logo

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -23,7 +23,7 @@ function Nav() {
     // </div>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#"><span>SH</span> Compliance & Service Consulting</a>
+        <a class="navbar-brand" href="#"><span id='sh-logo'>SH</span> Compliance & Service Consulting</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
