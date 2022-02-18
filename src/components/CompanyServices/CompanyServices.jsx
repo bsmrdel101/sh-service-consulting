784dbb95e8a1d3cb2b5a6ad7c9c0564c8144a637
Bennett Smrdel <smrdelb@gmail.com>
2022-02-18 12:38:39 -0600

    added images to the 3 services listed

--- src/components/CompanyServices/CompanyServices.jsx
+++ src/components/CompanyServices/CompanyServices.jsx
@@ -9,23 +9,23 @@ function CompanyServices() {
                 <h1 className='services-title'>Services</h1>
                 <div class="services-row">
                     <div class="col-lg-4">
-                        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" role="img" focusable="false"><rect width="100%" height="100%" fill="#777"></rect></svg>
+                        <img src='advancement.jpeg' height="200px" />
                 
-                        <h2>Heading</h2>
+                        <h2>Future Advancement</h2>
                         <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
                         <p><a class="btn btn-secondary" href="#">View details »</a></p>
                     </div>
                     <div class="col-lg-4">
-                        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" role="img" focusable="false"><rect width="100%" height="100%" fill="#777"></rect></svg>
+                        <img src='safety.jpeg' height="200px" />
                 
-                        <h2>Heading</h2>
+                        <h2>Safety</h2>
                         <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
                         <p><a class="btn btn-secondary" href="#">View details »</a></p>
                     </div>
                     <div class="col-lg-4">
-                        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" role="img" focusable="false"><rect width="100%" height="100%" fill="#777"></rect></svg>
+                        <img src='quality.jpeg' height="200px" />
                 
-                        <h2>Heading</h2>
+                        <h2>Quality Assurance</h2>
                         <p>And lastly this, the third column of representative placeholder content.</p>
                         <p><a class="btn btn-secondary" href="#">View details »</a></p>
                     </div>
