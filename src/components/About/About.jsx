53f9e4aaee521dd4955aba6f50463aabe19fa091
Bennett Smrdel <smrdelb@gmail.com>
2022-03-06 19:55:24 -0700

    merged about_text branch with master

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -14,7 +14,7 @@ function About() {
                     is performed with high quality to benefit your business. Our process is built on a relationship based of
                     excellence, integrity, and always trying to give the best performance for our clients. 
                     We will maintain regulatory expertise to ensure our service takes your business where it needs to be.
-
+ 
                     We can do our service in English or Spanish.
                 </p>
             </div>
