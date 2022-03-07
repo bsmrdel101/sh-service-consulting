7345b8e30c1706753de4dbe3aed859bf7f74f860
Bennett Smrdel <smrdelb@gmail.com>
2022-03-06 19:46:12 -0700

    changed the positioning of the about and services sections

--- src/components/CompanyServices/CompanyServices.jsx
+++ src/components/CompanyServices/CompanyServices.jsx
@@ -13,21 +13,18 @@ function CompanyServices() {
                 
                         <h2>Future Advancement</h2>
                         <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
-                        <p><a class="btn btn-secondary" href="#">View details »</a></p>
                     </div>
                     <div class="col-lg-4">
                         <img src='safety.jpeg' height="200px" />
                 
                         <h2>Safety</h2>
                         <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
-                        <p><a class="btn btn-secondary" href="#">View details »</a></p>
                     </div>
                     <div class="col-lg-4">
                         <img src='quality.jpeg' height="200px" />
                 
                         <h2>Quality Assurance</h2>
                         <p>And lastly this, the third column of representative placeholder content.</p>
-                        <p><a class="btn btn-secondary" href="#">View details »</a></p>
                     </div>
                 </div>
             </center>
