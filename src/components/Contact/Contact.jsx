e3d631904c15844434d176453cc889cedc1614cd
Bennett Smrdel <smrdelb@gmail.com>
2022-03-20 16:18:22 -0500

    added a clickable linkedin button that takes you to the linkedin page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,11 +7,11 @@ function ScheduleForm() {
         <center>
             <h1 className='services-title'>Contact</h1>
             <div>
-                <h4>Ruth Hansen</h4>
+                <h4 className='contact-name'>Ruth Hansen</h4>
                 <h5>(651)-964-5235</h5>
             </div>
             <p>SHcomplianceconsulting@gmail.com</p>
-            <img src='linkedin.png' className='contact-icon' />
+            <img src='linkedin.png' className='contact-icon' onClick={() => window.open("https://www.linkedin.com/in/ruth-hansen-b1b5a2167/")} />
         </center>
     );
 }
