63b977f30fb00bedd17b7390ef89eb083897492f
Bennett Smrdel <smrdelb@gmail.com>
2022-03-20 16:01:39 -0500

    added contact info to the DOM

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,8 +5,12 @@ import './Contact.css';
 function ScheduleForm() {
     return (
         <center>
-            <h1>Contact</h1>
-            
+            <h1 className='services-title'>Contact</h1>
+            <div>
+                <h4>Ruth Hansen</h4>
+                <h5>(651)-964-5235</h5>
+            </div>
+            <p>SHcomplianceconsulting@gmail.com</p>
         </center>
     );
 }
